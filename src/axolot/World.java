311162b94390b466491531530d12ce323675039e
Noe Delcroix <noe.delcroix.etu@univ-lille.fr>
2021-08-06 14:21:34 +0200

    un peu plus de debut

--- src/axolot/World.java
+++ src/axolot/World.java
@@ -1,28 +1,30 @@
 package axolot;
 
+import processing.core.PGraphics;
 
-public class World {
+public class World extends PGraphics{
 
-    private static WorldGenerator generator;
-    private static Player player;
+    private WorldGenerator generator;
+    private Player player;
 
-    public static void init() {
-        World.generator=new WorldGenerator();
+    public World(){
+        generator=new WorldGenerator();
         player=new Player(0,0);
 
         Blocks.init();
     }
 
-    public static void draw() {
-        World.tick();
-        World.render(player);
+
+    public void drawingLoop() {
+        this.tick();
+        this.render();
     }
 
-    public static void tick(){
+    public void tick(){
 
     }
 
-    public static void render(Player player){
-
+    public void render(){
+        background(255,0,0,0);
     }
 }
