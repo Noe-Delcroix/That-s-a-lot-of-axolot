ea278c241596d5df57d0ef1e8447f3641943cc3a
Noe Delcroix <noe.delcroix.etu@univ-lille.fr>
2021-08-06 11:47:02 +0200

    Initial commit

---
+++ src/axolot/Game.java
@@ -0,0 +1,21 @@
+package axolot;
+
+import processing.core.PApplet;
+
+public class Game extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("axolot.Game",args);
+    }
+
+    public void settings(){
+        fullScreen();
+    }
+
+    public void setup(){
+
+    }
+
+    public void draw(){
+
+    }
+}
